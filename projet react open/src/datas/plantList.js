527a7de757fe9818f17b10ca8d9caabc731ed5c9
naziradidier <nazirap20.aps1b@gmail.com>
2022-08-18 12:36:56 +0300

    mini moodle

---
+++ projet react open/src/datas/plantList.js
@@ -0,0 +1,100 @@
+import monstera from '../assets/monstera.jpg'
+import lyrata from '../assets/lyrata.jpg'
+import pothos from '../assets/pothos.jpg'
+import succulent from '../assets/succulent.jpg'
+import olivier from '../assets/olivier.jpg'
+import basil from '../assets/basil.jpg'
+import mint from '../assets/mint.jpg'
+import calathea from '../assets/calathea.jpg'
+import cactus from '../assets/cactus.jpg'
+
+export const plantList = [
+	{
+		name: 'Ampoule',
+		category: 'classique',
+		id: '1ed',
+		bestSale: true,
+		light: 2,
+		water: 3,
+		cover: monstera,
+		price: 15000
+	},
+	{
+		name: 'pomme frite avec poulet',
+		category: 'classique',
+		id: '2ab',
+		light: 3,
+		water: 1,
+		cover: lyrata,
+		price: 16000
+	},
+
+	{
+		name: 'lot bague',
+		category: 'classique',
+		id: '3sd',
+		light: 1,
+		water: 2,
+		cover: pothos,
+
+		price: 9000
+	},
+	{
+		name: 'iphoneXS de Nazirah',
+		category: 'classique',
+		id: '4kk',
+		light: 2,
+		water: 3,
+		cover: calathea,
+
+		price: 20000
+	},
+	{
+		name: 'Urbania',
+		category: 'extérieur',
+		id: '5pl',
+		light: 3,
+		water: 1,
+		cover: olivier,
+		price: 25000
+	},
+
+	{
+		name: 'Akanjo',
+		category: 'plante grasse',
+		id: '8fp',
+		light: 2,
+		water: 1,
+		cover: cactus,
+		price: 60000
+	},
+	{
+		name: 'Nazirah',
+		category: 'extérieur',
+		id: '7ie',
+		bestSale: true,
+		light: 2,
+		water: 3,
+		cover: basil,
+		price: 50000
+	},
+	{
+		name: 'Nazirah et son ipjoneXS',
+		category: 'plante grasse',
+		id: '9vn',
+		light: 2,
+		water: 1,
+		cover: succulent,
+		price: 80000
+	},
+
+	{
+		name: 'patte bolognaise',
+		category: 'extérieur',
+		id: '6uo',
+		light: 2,
+		water: 2,
+		cover: mint,
+		price: 40000
+	}
+]
